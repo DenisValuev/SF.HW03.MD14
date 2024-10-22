e7be8b1df46a7e512ba07330b161071bc4e82d1d
DenisValuev <valuev.denis91@mail.ru>
2024-10-22 11:25:03 +0300

    Добавьте файлы проекта.

---
+++ SF.HW03.MD14/Models/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF.HW03.MD14.Models
+{
+    internal class Contact
+    {
+        public string Name { get; }
+        public string LastName { get; }
+        public long PhoneNumber { get; }
+        public string Email { get; }
+
+        public Contact(string name, string lastName, long phoneNumber, string email)
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+}
